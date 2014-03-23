22e7220d2c93124dc6ecf12ad08352d944ada23b
Stanislav Vlasic <svlasic@gmail.com>
2014-03-23 18:24:36 +0100

    Merge with Pivos M1 kernel

--- drivers/amlogic/amports/vmpeg12.c
+++ drivers/amlogic/amports/vmpeg12.c
@@ -89,7 +89,10 @@ MODULE_AMLOG(LOG_LEVEL_ERROR, 0, LOG_LEVEL_DESC, LOG_DEFAULT_MASK_DESC);
 #define STAT_TIMER_ARM      0x10
 #define STAT_VDEC_RUN       0x20
 
-#define DEC_CONTROL_FLAG_FORCE_2500_576P_INTERLACE  0x0002
+#define DEC_CONTROL_FLAG_FORCE_2500_720_576_INTERLACE  0x0002
+#define DEC_CONTROL_FLAG_FORCE_3000_704_480_INTERLACE  0x0004
+#define DEC_CONTROL_FLAG_FORCE_2500_704_576_INTERLACE  0x0008
+#define DEC_CONTROL_FLAG_FORCE_2500_544_576_INTERLACE  0x0010
 
 static vframe_t *vmpeg_vf_peek(void*);
 static vframe_t *vmpeg_vf_get(void*);
@@ -230,12 +233,31 @@ static irqreturn_t vmpeg12_isr(int irq, void *dev_id)
             frame_prog = info & PICINFO_PROG;
         }
 
-        if ((dec_control & DEC_CONTROL_FLAG_FORCE_2500_576P_INTERLACE) &&
+        if ((dec_control & DEC_CONTROL_FLAG_FORCE_2500_720_576_INTERLACE) &&
             (frame_width == 720) &&
             (frame_height == 576) &&
             (frame_dur == 3840)) {
             frame_prog = 0;
         }
+        else if ((dec_control & DEC_CONTROL_FLAG_FORCE_3000_704_480_INTERLACE) &&
+            (frame_width == 704) &&
+            (frame_height == 480) &&
+            (frame_dur == 3200)) {
+            frame_prog = 0;
+        }
+        else if ((dec_control & DEC_CONTROL_FLAG_FORCE_2500_704_576_INTERLACE) &&
+            (frame_width == 704) &&
+            (frame_height == 576) &&
+            (frame_dur == 3840)) {
+            frame_prog = 0;
+        }
+        else if ((dec_control & DEC_CONTROL_FLAG_FORCE_2500_544_576_INTERLACE) &&
+            (frame_width == 544) &&
+            (frame_height == 576) &&
+            (frame_dur == 3840)) {
+            frame_prog = 0;
+        }
+
 
         if (frame_prog & PICINFO_PROG) {
             u32 index = ((reg & 7) - 1) & 3;
--- drivers/amlogic/mali/__malidrv_build_info.c
+++
@@ -1,1 +0,0 @@
-char *__malidrv_build_info(void) { return "r2p4-00dev0";}
